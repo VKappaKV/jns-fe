416a16f6fb68e66b541bf025756b79a637d736ea
giandonato.inverso@edempg.it <giandonato.inverso@studenti.unimi.it>
2023-10-24 15:41:00 +0200

    renamed app

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { render } from "react-dom";
-import App from './App';
+import App from './Phantom';
 import App2 from "./App2";
 
 /* multi chain sandbox */
