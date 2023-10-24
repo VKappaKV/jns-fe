ddff50e3b45fa3fdaf35461a5538be231663a29f
giandonato.inverso@edempg.it <giandonato.inverso@studenti.unimi.it>
2023-10-24 15:54:37 +0200

    Pulizia del branch principale

--- src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as detectPhantomMultiChainProvider } from './detectPhantomMultiChainProvider';
-export { default as getChainName } from './getChainName';
-export { default as hexToRGB } from './hexToRGB';
-export { default as numToHexString } from './numToHexString';
-export { default as signMessageOnEthereum } from './signMessageOnEthereum';
-export { default as signMessageOnSolana } from './signMessageOnSolana';
-export { default as switchEthereumChain } from './switchEthereumChain';
